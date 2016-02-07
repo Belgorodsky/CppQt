60cafc6d388a3d68b5d7f3ac4d3c7af5b850019e
Yarique Belgorodsky <spec1al.yarik@gmail.com>
2016-02-07 19:03:20 +0300

    wait and QThread

---
+++ Waiter/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <QCoreApplication>
+#include "mythread.h"
+#include <QDebug>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    MyThread thread;
+
+     thread.start();
+     thread.wait();
+     qDebug() << "Done!";
+
+    return a.exec();
+}
---
+++ Waiter/mythread.cpp
@@ -0,0 +1,16 @@
+﻿#include "mythread.h"
+#include <QDebug>
+
+MyThread::MyThread(QObject *parent) : QThread(parent)
+{
+
+}
+
+void MyThread::run()
+{
+    for( int i = 0; i < 1e1; ++i)
+    {
+        qDebug() << i;
+        msleep(500);
+    }
+}
